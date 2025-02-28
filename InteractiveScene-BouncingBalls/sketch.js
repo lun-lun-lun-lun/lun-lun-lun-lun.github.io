3aed91a3787a721d07192dc9450766884f9d7ad6
lun-lun-lun-lun <devlunlun@gmail.com>
2025-02-28 14:15:12 -0600

    Updated vector stuff

--- InteractiveScene-BouncingBalls/sketch.js
+++ InteractiveScene-BouncingBalls/sketch.js
@@ -8,19 +8,39 @@
 // - framerate set
 // - deltaTime shenaniganery such that it doesnt calculate wrong when you lag
 // - scroll wheel input
+// - docstrings
 
 let balls = [];
-let current_ball_radius = 20;
+let mouse_ball_radius = 20;
 let grabbed_ball;
 
 let windowWidth = 400;
 let windowHeight = 400;
 
 let radius;
+const gravity = 5.3;
 
 
+/**
+ * Converts degrees into a an array of X and Y.
+ * @param {number} degrees the angle in degrees
+ * @returns { [number, number] } the x and y.
+ * Recommended you let [x, y] = this_func
+ */
+function DegreesToVectors(degrees) {
+
+  let vector_x = Math.cos(degrees);
+  let vector_y = Math.sin(degrees);
+  
+  return [vector_x, vector_y];
+  
+}
 
 
+/**
+ * Has an x and y vector.
+ * @class
+ */
 class Vector2 {
   constructor(x, y) { //, direction, power, weight, radius, name) {
     //direction is from 0 to 359.999, it is in degrees
@@ -28,49 +48,72 @@ class Vector2 {
     this.y = y;
     
   }
-  get direction() {
 
-  }
-  CalcDirectionAngle(in_degrees) {
-    // x/y = x_ratio
+  /**
+ * Returns the direction angle of itself, Vector2.
+ * @returns {number} The angle, in degrees.
+ */
+  get angle() {
     let angle_radians = Math.atan2(y, x);
     let angle_degrees = angle_radians * 180 / Math.PI;
-    x_ratio = this.x / this.y;
 
+    return return_degrees ? angle_degrees : angle_radians;
   }
-  DoMath(operator, math_number) {
+
+  /**
+ * Does the math you specify on X AND Y of the Vector2.
+ * @param {"*"|"x"|"+"|"-"|"/"|"^"|"%"} operator The operation you want to perform.
+ * @param {number} math_number The number you want to do it with.
+ */
+  MathSelf(operator, math_number) { //there has to be a better way to do this :sob:
     if (operator === "*" || operator === "x") { 
-      return new Vector2(this.x * math_number, this.y * math_number);
+      this.x *= math_number; this.y * math_number;
     }
     else if (operator === "+") {
-      return new Vector2(this.x + math_number, this.y + math_number);
+      this.x += math_number; this.y += math_number;
     }
     else if (operator === "-") {
-      return new Vector2(this.x * math_number, this.y * math_number);
+      this.x -= math_number; this.y -= math_number;
     }
     else if (operator === "/") {
-      return new Vector2(this.x / math_number, this.y / math_number);
+      this.x -= math_number; this.y -= math_number;
     }
-    else if (operator === "^") {
-      return new Vector2(this.x ^ math_number, this.y ^ math_number);
+    else if (operator === "^") { //exponent
+      this.x ^= math_number; this.y ^= math_number;
     }
     else if (operator === "%") { //remainder
-      return new Vector2(this.x % math_number, this.y % math_number);
+      this.x ^= math_number; this.y ^= math_number;
     }
+  }
+
+
+  /**
+ * Change the direction of the vector, while keeping the power the same.
+ * @param {number} direction The operation you want to perform.
+ * If you want to go in the opposire direction, dir should be this.angle + 180
+ */
+  ChangeDirection(direction) {
+
+    //get direction that the new vector will be in
+    let [new_x, new_y] = DegreesToVectors(direction);
+
+
+    let strong_old_x = this.x;
+    let old_angle = this.angle;
+    let [angle_x, _] = DegreesToVectors(multiplied_angle);
+    let old_strength = multiplied_x/angle_x;
+
+    //multiply the new vector by the strength of the old vector
+    this.x = new_x; this.y = new_y; 
+    this.MathSelf("*", old_strength);
   }
   
 }
 
 
-function DegreesToDirection(degrees) {
 
-  let vector_x = Math.cos(degrees);
-  let vector_y = Math.sin(degrees);
-  
 
-  vector = new Vector2( vector_x, vector_y );
-  
-}
+
 
 
 
@@ -88,7 +131,6 @@ class Ball {
     this.time_existed = 0;
     this.noBounce = false;
     
-    //                [x,y]
     this.total_velo = new Vector2(0,0);
   }
 
@@ -110,15 +152,15 @@ function setup() {
 }
 
 //resize balls if you use up or down keys
-function updateBallSize() {
+function UpdateMouseBall() {
   
   
   if (keyIsPressed === true) {
     if (keyCode === UP_ARROW) {
-      current_ball_radius += 0.35;
+      mouse_ball_radius += 0.35;
     } 
     else if (keyCode === DOWN_ARROW) {
-      current_ball_radius -= 0.35;
+      mouse_ball_radius -= 0.35;
     }
   }
 }
@@ -134,8 +176,11 @@ function updateBallPosition(ball) {
   
 }
 
-//draw the balls on the screen
-function drawBalls() {
+/**
+ * Draws all the balls onto the screen
+ * Returns nothing
+ */
+function DrawBalls() {
   
   for (let ball of balls) {
     
@@ -157,18 +202,21 @@ function draw() {
   
   fill(25,30,40,50);
   noStroke();
-  circle(mouseX, mouseY, current_ball_radius*2);
-  
-  
+  circle(mouseX, mouseY, mouse_ball_radius*2);
+  DrawBalls();
+  UpdateMouseBall();
+  UpdateBallVelocity();
+
+
   //draw each ball at their respective positions
   
   // if (keyIsDown(87)) {
   //   let ball_name = "ball_" + balls.length.toString();
-  //   spawnBall(mouseX, mouseY, 0, 0, 200, current_ball_radius, ball_name);
+  //   spawnBall(mouseX, mouseY, 0, 0, 200, mouse_ball_radius, ball_name);
   // }
-  drawBalls();
   
-  updateBallSize(); 
+  
+  
 }
 
 function spawnBall(x_pos, y_pos, x_velo, y_velo, weight, radius, name) {
