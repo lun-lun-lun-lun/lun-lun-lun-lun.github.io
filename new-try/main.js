9f2650a589d6c1d4af29cd8596176128fd65600b
lun-lun-lun-lun <devlunlun@gmail.com>
2025-03-12 14:16:03 -0600

    Updates main.js, players are now added to a table with a unique name

--- new-try/main.js
+++ new-try/main.js
@@ -1,26 +1,58 @@
+// -todo
+//get players moving on the screen
+//when im done I need to annotate all the funcs with what they do
+//dont do typecheck early like last time
+
+
+
 let shared;
+//i do not expect 456 people. its just canon to the show.
+const MAX_PLAYERS = 456;
+//ctrl i for modmenu
 //p5.party doesnt work in this way, but this close to the way that im used to
 //note to self: make squid game next time?
 let playerTemplate = {
-    id: 0,
-    x_position: 0,
-    y_position: 0,
+    id:             0,
+    name:           "",
+    x_position:     0,
+    y_position:     0,
     moveSpeed:      100,
     currentHealth:  100,
     maxHealth:      100,
-    swordLength:    100,
+    strength:       100,
+    // swordLength:    100,
     
-    upgrades: {
-      //maximu
-      health:         0,
-      range:          0,
-      speed:          0,
-      damage:         0,
-      defense:        0,
-    }
+    // upgrades: {
+    //   //maximu
+    //   health:         0,
+    //   range:          0,
+    //   speed:          0,
+    //   damage:         0,
+    //   defense:        0,
+    // }
     
 }
 
+//gets a unique name from a list
+function getUniqueName(folder) {
+
+  let name = toString(random(1,MAX_PLAYERS));
+
+  for (let person of folder) {
+    if (person["name"] === name) {
+      return getUniqueName(folder)
+    }
+  }
+
+  let length = name.length;
+  if (length === 2) {
+    name = "0"+name
+  } else if (length === 1) {
+    name = "00"+name
+  }
+
+  return name
+}
 
 
 function preload() {
@@ -37,13 +69,11 @@ function preload() {
         });
 	}
     shared = partyLoadShared(
-    "shared", 
+    "global", 
     { 
-        x: 100, 
-        y: 100,
-        players: {
-          "ol": 1,
-        },
+        x: 5, 
+        y: 5,
+        players: {},
     }
 );
 }
@@ -51,15 +81,32 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   noStroke();
-  console.log(partyIsHost())
-  let totalPlayers = Object.keys(shared.players).length;
-  let newPlayer = structuredClone(playerTemplate);
-  newPlayer.id = totalPlayers+1
+
   
-  shared.players[totalPlayers+1] = newPlayer
+
+  
+
+
+  
+  // let newPlayer = structuredClone(playerTemplate);
+  // newPlayer.id = totalPlayers+1
+  
+  // shared.players[totalPlayers+1] = newPlayer
+}
+
+function addNewUser() {
+  let players = shared.players;
+  let amountPlayers = Object.keys(players).length;
+  let id = amountPlayers+1
+  let newUser = structuredClone(playerTemplate);
+
+  newUser.name = getUniqueName(folder)
+  players[id] = newUser
 }
 
 function mousePressed() {
+  console.log(partyIsHost())
+  console.log(shared.players)
   // shared.x = mouseX;
   // shared.y = mouseY;
   // // shared.players = {
