10af0e30755e64d416246c65d588500f5d975336
lun-lun-lun-lun <devlunlun@gmail.com>
2025-03-31 14:14:04 -0600

    Can move around and view a cube in 3D

--- multiplayer-flying/main.js
+++ multiplayer-flying/main.js
@@ -1,26 +1,28 @@
-// Red Light Green Light
+// Flying
 // Laeron Lewis
-// March 20th, 2025
+// March 31st, 2025
 
 // Extra For Experts:
-// held and release keys
-// does const count? i forgot if we covered it
-// Screen scrolling
+// 3D
 // Multiplayer
-// sadly no docstrings because i am Lazy
 
 // To Do:
-// add onRelease, reoganize keybinds, add pushing, make the actual minigame
-// should make red pop up on the screen, but get less and less visible each time it pops up
-// short scene for players that lose
+// Add input system (simply it - didn't need allat from before)
+// Characters, moving a model
+// Towers, Walls
+// Smooth movement & physics (try to port from before)
+// Human-Object Collisions
+// Object-Object physics and collisions?? (try to port from DeltaPhysics)
 
 
 let shared;
 
 let idk = [];
 let me, guests;
-let playerCamera;
-
+let myCamera;
+let dragX = 0;
+let dragY = 0;
+let flySpeed = 2
 
 //detect players leaving the game
 window.addEventListener("beforeunload", function (e) {
@@ -57,10 +59,12 @@ function preload() {
 }
 
 function setup() {
+  //enable 3D rendering mode on the canvas
   createCanvas(windowWidth*0.8, windowHeight*0.8, WEBGL);
-  //noStroke();
 
-  playerCamera = createCamera();
+  myCamera = createCamera();
+
+  //camera(mouseX, mouseY, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
 }
 
 
@@ -68,6 +72,7 @@ function setup() {
 function mousePressed() {
   console.log(me)
   console.log(partyLoadGuestShareds())
+  requestPointerLock()
 }
 
 //this could leave some things undetected if players join 
@@ -77,15 +82,36 @@ function draw() {
   
   // https://github.com/processing/p5.js/wiki/Getting-started-with-Webgl-in-p5
   // https://processing.org/tutorials/p3d
-  translate(-width/2,-height/2,-100);
-  box();
-
+  //background(0);
+  mouseCaptured = false
+    myCamera.pan(-movedX * 0.005)
+    myCamera.tilt(movedY * 0.005)
+    myCamera.move(
+      // D - right, A - left 
+      (keyIsDown(68) ? flySpeed : 0) + (keyIsDown(65) ? -flySpeed : 0),
+      // Q - down, E - up
+      (keyIsDown(81) ? flySpeed : 0) + (keyIsDown(69) ? -flySpeed : 0),
+      // S - backward, W - forward
+      (keyIsDown(83) ? flySpeed : 0) + (keyIsDown(87) ? -flySpeed : 0)
+    );
+    box(200);
+  
+  // camera(mouseX, height/2, (height/2) / tan(PI/6), mouseX, height/2, 0, 0, 1, 0);
+  // translate(width/2, height/2, -100);
+  // stroke(255);
+  // noFill();
+  
 
   //drawText()
 
 }
 
+function mouseDragged() {
+  
 
+  
+  //camera(dragX, mouseY, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
+}
 
 
 
